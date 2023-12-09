857fd1a23676fac214cd1f1d4de2b37e380519c2
3090 <thisyzg@outlook.com>
2023-12-09 19:55:18 +0000

    What's New:  - Add new data generation.

--- dataset/gen_data.py
+++ dataset/gen_data.py
@@ -13,26 +13,55 @@ def load_text(dic):
     return dic["text"]
 
 
-folder = "../data/json"
-folder_out = "../data/txt"
+def gen_gpt():
+    folder = "../data/json"
+    folder_out = "../data/txt"
 
-for file in os.listdir(folder):
-    if file.endswith(".jsonl"):
-        print(file)
-        # file = "small-117M.test.jsonl"
-        # replace the extension with .txt
-        txt_file = file.replace(".jsonl", ".txt")
-        txt_lines = []
+    for file in os.listdir(folder):
+        if file.endswith(".jsonl"):
+            print(file)
+            # file = "small-117M.test.jsonl"
+            # replace the extension with .txt
+            txt_file = file.replace(".jsonl", ".txt")
 
-        # open the file and write the text only
-        with open(os.path.join(folder_out, txt_file), "w") as ft:
-            # open the jsonl file
-            with open(os.path.join(folder, file), "r") as f:
-                lines = f.readlines()
-                for line in lines:
-                    line_dict = json.loads(line)  # convert to dict
-                    text = load_text(line_dict)  # get the text only
-                    text = text.replace("\n", " ")  # remove new line,
-                    # insert the \t in the middle of the text
-                    text = text[: len(text) // 2] + "\t" + text[len(text) // 2:]
-                    ft.write(text + "\n")
+            # open the file and write the text only
+            with open(os.path.join(folder_out, txt_file), "w") as ft:
+                # open the jsonl file
+                with open(os.path.join(folder, file), "r") as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        line_dict = json.loads(line)  # convert to dict
+                        text = load_text(line_dict)  # get the text only
+                        text = text.replace("\n", " ")  # remove new line,
+                        # insert the \t in the middle of the text
+                        text = text[: len(text) // 2] + "\t" + text[len(text) // 2:]
+                        ft.write(text + "\n")
+
+
+def gen_wiki():
+    folder = "../data/wikitext-2-raw"
+    folder_out = "../data/txt"
+
+    for file in os.listdir(folder):
+        if file.endswith(".raw"):
+            print(file)
+            txt_file = file.replace(".raw", ".txt")
+
+            # open the file and write the text only
+            with open(os.path.join(folder_out, txt_file), "w") as ft:
+                # open the jsonl file
+                with open(os.path.join(folder, file), "r") as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        if line.strip() == '' or line.strip()[0] == '=':
+                            continue
+                        text = line
+                        text = text.replace("\n", " ")  # remove new line,
+                        # insert the \t in the middle of the text
+                        text = text[: len(text) // 2] + "\t" + text[len(text) // 2:]
+                        ft.write(text + "\n")
+
+
+if __name__ == "__main__":
+    gen_wiki()
+
